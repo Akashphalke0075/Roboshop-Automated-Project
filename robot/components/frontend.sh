27de43ef35485e12603c7fd38b5da0f82dad7237
Akash <phalke.aka@gmail.com>
2023-03-29 19:49:28 +0530

    update

---
+++ robot/components/frontend.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -e "\e[32m installing nginx \e[0m"
+yum install nginx -y
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# systemctl enable nginx
+# systemctl start nginx
+
+
+
