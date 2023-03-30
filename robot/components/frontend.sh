24f7f629f8dbdb7c8d0a092142d78d5b019648b1
Akash <phalke.aka@gmail.com>
2023-03-30 17:08:51 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,54 +1,72 @@
 #!/bin/bash
 
-set -e 
-
+set -e
 COMPONENT=frontend
-LOGFILE=/tmp/$COMPONENT.log
+LOGFILE=/tmp/frontend.log
 USERID=$(id -u) 
 
 if [ $USERID -ne 0 ]; then
-echo -e "\e[31m please run as sudo user or root \e[0m"
+echo "please login as sudo user"
 exit 1
 fi
 
-stat () {
-if [ $1 -eq  0 ]; then
-echo -e "\e[32msuccess \e[0m"
+stat() {
+if [ $? -eq 0 ]; then
+echo -e "\e[32mSuccess \e[0m"
 else
-echo -e "\e[31mfailure \e[0m"
+echo -e "\e[31mFailure \e[0m"
 fi
 }
 
 echo -n "installing nginx: "
-yum install nginx -y   &>> $LOGFILE
+yum install nginx -y  &>> $LOGFILE
 stat $?
 
 
-echo -n "downloading component: "
+echo -n "downloading the component: "
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
 stat $?
 
-echo -n "cleaunup performing: "
-rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+echo -n "cleaning up the component: "
+rm -rf /usr/share/nginx/html*  &>> $LOGFILE
 stat $?
 
+echo -n "unzipping the component adn moving : "
 cd /usr/share/nginx/html
-echo -n "unzipping components: "
 unzip /tmp/$COMPONENT.zip  &>> $LOGFILE
-stat $?
-
-echo -n "moving components: "
 mv $COMPONENT-main/* .
 mv static/* .
 rm -rf $COMPONENT-main README.md
 stat $?
 
-echo -n "configuring reverse proxy: "
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "unzipping the component adn moving : "
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  
 stat $?
 
-echo -n "installing nginx: "
+
+echo -n "starting nginx : "
 systemctl enable nginx  &>> $LOGFILE
 systemctl start nginx   &>> $LOGFILE
 stat $?
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# systemctl enable nginx
+# systemctl start nginx
