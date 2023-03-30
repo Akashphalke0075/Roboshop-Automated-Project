e3278859a22176659e855d54435eebe9cd259568
Akash <phalke.aka@gmail.com>
2023-03-30 15:21:23 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -2,29 +2,49 @@
 
 set -e 
 
-USERID=$(id -u)
+COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
+USERID=$(id -u)  &>> $LOGFILE
 if [ $USERID -ne 0 ]; then
 echo -e "\e[31m please run as sudo user or root \e[0m"
 exit 1
 fi
 
-echo -e "\e[32m installing nginx \e[0m"
-yum install nginx -y  
+echo -n "installing nginx: "
+yum install nginx -y   &>> $LOGFILE
+stat $?
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat () {
+if [ $1 -eq  0 ]; then
+echo -e "\e[32msuccess \e[0m"
+else
+echo -e "\e[31mfailure \e[0m"
+fi
+}
 
-rm -rf /usr/share/nginx/html/*
+echo -n "downloading component: "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+stat $?
+
+echo -n "cleaunup performing: "
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 cd /usr/share/nginx/html
+stat $?
 
-unzip /tmp/frontend.zip
-mv frontend-main/* .
+echo -n "unzipping components: "
+unzip /tmp/$COMPONENT.zip  &>> $LOGFILE
+echo -n "moving components: "
+mv $COMPONENT-main/* .
 mv static/* .
-rm -rf frontend-main README.md
+rm -rf $COMPONENT-main README.md
+stat $?
+
+echo -n "configuring reverse proxy: "
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
-
-
-systemctl enable nginx
-systemctl start nginx
-
+echo -n "installing nginx: "
+systemctl enable nginx  &>> $LOGFILE
+systemctl start nginx   &>> $LOGFILE
+stat $?
 
