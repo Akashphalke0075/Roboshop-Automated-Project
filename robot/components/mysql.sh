fe362652649972fede3b4df53219b73dcd513ad9
Akash <phalke.aka@gmail.com>
2023-03-31 15:09:42 +0530

    update

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -17,34 +17,40 @@ echo -n "starting mysql : "
 systemctl enable mysqld && systemctl start mysqld &>> $LOGFILE
 stat $?
 
-echo -n "Changing the default password:"
-DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
-
-# I only want to change the default password only for the first time.
-# How do I come to know whether the password is changed or not.
-echo show databases | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE 
-if [ $? -ne 0 ] ; then 
-    echo -n "Reset Root Password"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql  --connect-expired-password  -uroot -p"${DEF_ROOT_PASSWORD}" &>> $LOGFILE 
-    stat $? 
-fi 
-
-echo show plugins | mysql -uroot -p${MYSQL_PWD} | grep validate_password; &>> $LOGFILE 
-if [ $? -eq 0 ] ; then 
-    echo -n "Uninstalling Password Validate Plugin: "
-    echo "uninstall plugin validate_password;" | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE 
-    stat $? 
-fi 
-
-echo -n "Downloading the $COMPONENT Schema:"
-cd /tmp 
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-unzip -o $COMPONENT.zip &>> $LOGFILE
-stat $?
-
-echo -n "Injecting the $COMPONENT Schema:"
-cd /tmp/$COMPONENT-main/
-mysql -uroot -p${MYSQL_PWD} < shipping.sql &>> $LOGFILE
+echo -n "Grab $COMPONENT default password :"
+DEFAULT_ROOT_PWD=$(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 stat $? 
 
-echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"
+# This should only run for the first time or when the default password is not changed.
+echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE 
+if [ $? -ne 0 ] ; then 
+
+    echo -n "Password Reset of root user :"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE 
+    stat $?
+
+fi 
+
+# Ensure you run this only when the password validate plugin exist 
+echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>> $LOGFILE 
+if [ $? -eq 0 ] ; then 
+
+    echo -n "Uninstalling Password Validation Plugin :"
+    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
+    stat $?
+
+fi 
+
+echo -n "Downloading the $COMPONENT Schema :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+stat $? 
+
+echo -n "Extracting the $COMPONENT Schema :"
+cd /tmp 
+unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $? 
+
+echo -n "Injecting the schema :"
+cd $COMPONENT-main
+mysql -uroot -pRoboShop@1 < shipping.sql 
+stat $? 
