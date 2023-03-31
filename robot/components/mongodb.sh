6e4eb7d968bac6adcdf5910c5d2e2df5e4cd8759
Akash <phalke.aka@gmail.com>
2023-03-31 08:54:10 +0530

    update

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
 set -e
-mongodb=$COMPONENT
-LOGFILE=/tmp/$COMPONENT.log
-USERID=$(id -u) 
+COMPONENT=mongodb
 
-if [ $USERID -ne 0 ]; then
-echo -e "\e[31mplease login as sudo \e[0m"
-exit 1
-fi
+source components/common.sh
 
-stat() {
-if [ $? -eq 0 ]; then
-echo -e "\e[32mSuccess \e[0m"
-else
-echo -e "\e[31mFailure \e[0m"
-fi
-}
+echo -n "downloading components: "
+curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo  &>> $LOGFILE
+stat $?
 
-# curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+echo -n "installing $COMPONENT: "
+yum install -y $COMPONENT-org   &>> $LOGFILE
+stat $?
 
-# yum install -y mongodb-org
-# systemctl enable mongod
-# systemctl start mongod
+echo -n "listening the port: "
+sed -e -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  
+stat $?
+
+echo -n "downloading the component: "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+stat $?
+
+echo -n "downloading the component: "
+cd /tmp
+unzip $COMPONENT.zip  &>> $LOGFILE
+stat $?
+
+echo -n "ijnecting schema: "
+cd $COMPONENT-main
+mongo < catalogue.js  &>> $LOGFILE
+mongo < users.js   &>> $LOGFILE
+stat $?
+
+
+echo -n "starting $COMPONENT : "
+systemctl enable mongod &>> $LOGFILE
+systemctl start mongod &>> $LOGFILE
+stat $?
+
 
 
 
