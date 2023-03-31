b4d72060be46ca6c107f23063085b131fa026b98
Akash <phalke.aka@gmail.com>
2023-03-31 14:30:39 +0530

    update

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -5,54 +5,40 @@ COMPONENT=catalogue
 APPUSER=roboshop
 source components/common.sh
 
-echo -n "downloading components: "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
-stat $?
+NODEJS
 
-echo -n "installing nodejs: "
-yum install nodejs -y  &>> $LOGFILE 
-stat $?
+# echo -n "downloafing $COMPONENT: "
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+# stat $?
 
+# echo -n "unzipping the component adn moving: "
+# cd /home/$APPUSER
+# unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
+# stat $?
 
-echo -n "creating user: "
-id $APPUSER &>> $LOGFILE
-if [ $? -ne 0 ]; then
-useradd $APPUSER &>> $LOGFILE
-fi
-stat $?
+# echo -n "performing cleaup: "
+# rm -rf $COMPONENT
+# mv $COMPONENT-main $COMPONENT
+# stat $?
 
-echo -n "downloafing $COMPONENT: "
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
-stat $?
+# echo -n "installing npm: "
+# cd /home/$APPUSER/$COMPONENT
+# npm install &>> $LOGFILE
+# stat $?
 
-echo -n "unzipping the component adn moving: "
-cd /home/$APPUSER
-unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-stat $?
+# echo -n "Changing permissions: "
+# chown -R $APPUSER:$APPUSER  /home/$APPUSER/$COMPONENT
+# stat $
 
-echo -n "performing cleaup: "
-rm -rf $COMPONENT
-mv $COMPONENT-main $COMPONENT
-stat $?
+# echo -n "Configuring dns name: "
+# sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/'  /home/$APPUSER/$COMPONENT/systemd.service 
+# mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+# stat $?
 
-echo -n "installing npm: "
-cd /home/$APPUSER/$COMPONENT
-npm install &>> $LOGFILE
-stat $?
+# echo -n "starting nginx : "
+# systemctl daemon-reload &>> $LOGFILE
+# systemctl start $COMPONENT &>> $LOGFILE
+# systemctl enable $COMPONENT &>> $LOGFILE
+# stat $?
 
-echo -n "Changing permissions: "
-chown -R $APPUSER:$APPUSER  /home/$APPUSER/$COMPONENT
-stat $
-
-echo -n "Configuring dns name: "
-sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/'  /home/$APPUSER/$COMPONENT/systemd.service 
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
-
-echo -n "starting nginx : "
-systemctl daemon-reload &>> $LOGFILE
-systemctl start $COMPONENT &>> $LOGFILE
-systemctl enable $COMPONENT &>> $LOGFILE
-stat $?
-
-echo -e "\e[32m..............catalogue completed....... \e[0m"
+echo -e "\e[32m..............$COMPONENT completed....... \e[0m"
--- robot/components/common.sh
+++ robot/components/common.sh
@@ -15,3 +15,68 @@ else
 echo -e "\e[31mFailure \e[0m"
 fi
 }
+
+NODEJS() {
+    echo -n "downloading components: "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+stat $?
+
+echo -n "installing nodejs: "
+yum install nodejs -y  &>> $LOGFILE 
+stat $?
+#CALLING CREATE USER
+CREATE_USER
+#CALLING DOWNLOADINGING AND EXTRACT
+DOWNLOAD_EXTRACT
+#CALLING NPM DEPENDENCIES
+NPM_INSTALL
+#CALLING CONFIGURE SERVICE
+CONFIGURE_SERVICE
+}
+
+CREATE_USER() {
+    echo -n "creating user: "
+id $APPUSER &>> $LOGFILE
+if [ $? -ne 0 ]; then
+useradd $APPUSER &>> $LOGFILE
+fi
+stat $?
+}
+
+DOWNLOAD_EXTRACT() {
+    echo -n "downloafing $COMPONENT: "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> $LOGFILE
+stat $?
+
+echo -n "performing cleaup: "
+rm -rf /home/$APPUSER/$COMPONENT
+stat $?
+
+echo -n "unzipping the component adn moving: "
+cd /home/$APPUSER
+unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE && mv $COMPONENT-main $COMPONENT  &>> $LOGFILE 
+stat $?
+
+echo -n "Changing permissions: "
+chown -R $APPUSER:$APPUSER  /home/$APPUSER/$COMPONENT
+stat $ 
+}
+
+NPM_INSTALL() {
+    echo -n "installing npm: "
+cd /home/$APPUSER/$COMPONENT
+npm install &>> $LOGFILE
+stat $?
+}
+
+CONFIGURE_SERVICE() {
+echo -n "Configuring dns name: "
+sed -i -e 's/MONGO_DNSNAME/mongodb.robot.internal/'  /home/$APPUSER/$COMPONENT/systemd.service 
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+stat $?
+echo -n "starting nginx : "
+systemctl daemon-reload &>> $LOGFILE
+systemctl start $COMPONENT &>> $LOGFILE
+systemctl enable $COMPONENT &>> $LOGFILE
+stat $?
+}
