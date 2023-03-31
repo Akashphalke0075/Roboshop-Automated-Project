a095a32f2ccc1bd12a1829cbae5aa82725ce53e2
Akash <phalke.aka@gmail.com>
2023-03-31 10:31:43 +0530

    update

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -13,5 +13,19 @@ echo -n "installing $COMPONENT: "
 yum install nodejs -y  &>> $LOGFILE  &>> $LOGFILE
 stat $?
 
-# echo -n "creating user: "
-# useradd roboshop
+echo -n "creating user: "
+useradd roboshop 
+stat $?
+
+echo -n "downloafing $COMPONENT: "
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"  &>> $LOGFILE
+
+echo -n "unzipping the component adn moving: "
+cd /home/roboshop
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
+mv catalogue-main catalogue
+stat $?
+
+cd /home/roboshop/catalogue
+npm install &>> $LOGFILE
+stat $?
