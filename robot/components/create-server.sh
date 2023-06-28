272117e37040e91c9d268679a15aac74f82974ac
Akash <phalke.aka@gmail.com>
2023-06-28 13:25:47 +0530

    update

--- robot/components/create-server.sh
+++ robot/components/create-server.sh
@@ -7,7 +7,7 @@ fi
 
 COMPONENT=$1
 # ENV=$2
-ZONE_ID="Z08945451O67A5VIDZSZL"
+ZONE_ID="Z07096621M8RAUJIGSIQU"
 
 # AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 AMI_ID="ami-0c1d144c8fdd8d690"
